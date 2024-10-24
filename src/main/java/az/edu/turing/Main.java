e23be39cb7ea1c3b0d749ef3b8df7dd6d450827d
seyran7 <brsr888@gmail.com>
2024-10-24 20:36:57 +0400

    new cod

--- src/main/java/az/edu/turing/Main.java
+++ src/main/java/az/edu/turing/Main.java
@@ -1,7 +1,11 @@
 package az.edu.turing;
 
 public class Main {
-    public static void main(String[] args) {
+  public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.print("hello world!");
+        System.out.printf("hello, %s","World");
+        System.out.printf("Hello,World: %.2f",3.14);
+        System.out.println("1\n2\n,4\n,5\n");
     }
 }
