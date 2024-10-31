fb2ae8f6044caaf2995090bc5dedb1a1e7dab0f7
seyran7 <brsr888@gmail.com>
2024-10-31 21:14:47 +0400

    initial commit

--- src/main/java/az/edu/turing/Main.java
+++ src/main/java/az/edu/turing/Main.java
@@ -1,37 +1,44 @@
 package az.edu.turing;
-
+import java.util.Scanner;
 public class Main {
   public static void main(String[] args) {
-       // System.out.println("Hello world!");
-       //  System.out.print("hello world!\n");
-       // System.out.printf("hello, %s","World\n");
-       //  System.out.printf("Hello,World: %.2f",3.14);
-       //  System.out.println("\n1\n2\n3\n4\n5\n");
-    //  int number=32;
-     // number=43;
-   //   System.out.println(number);
-            //  byte numberOne=127;
-          //    short  numberTwo=32767;
-        //      int numberThree=2147483647;
-        //      long numberFour=9223372036854775807L;
-   //   System.out.println(numberOne);
-  //    System.out.println(numberTwo);
-     // System.out.println(numberThree);
-  //    System.out.println(numberFour);
-    //  System.out.print(numberOne);
-    //  System.out.print(numberTwo);
-    //  System.out.print(numberThree);
-   //   System.out.print(numberFour);
-      int a=16;
-      int b=8;
-      int i=a+b;
-      int h=a-b;
-      int g=a/b;
-      int q=a*b;
-      System.out.println(i);
-      System.out.println(h);
-      System.out.println(g);
-      System.out.println(q);
+        System.out.println("Hello world!");
+         System.out.print("hello world!\n");
+        System.out.printf("hello, %s","World\n");
+         System.out.printf("Hello,World: %.2f",3.14);
+         System.out.println("\n1\n2\n3\n4\n5\n");
+     int number=32;
+      number=43;
+     System.out.println(number);
+             byte numberOne=127;
+             short  numberTwo=32767;
+             int numberThree=2147483647;
+             long numberFour=9223372036854775807L;
+      System.out.println(numberOne);
+      System.out.println(numberTwo);
+      System.out.println(numberThree);
+      System.out.println(numberFour);
+      System.out.print(numberOne);
+      System.out.print(numberTwo);
+      System.out.print(numberThree);
+      System.out.print(numberFour);
+         int a=16;
+         int b=8;
+         int i=a+b;
+         int h=a-b;
+         int g=a/b;
+         int q=a*b;
+     System.out.println(i);
+     System.out.println(h);
+     System.out.println(g);
+     System.out.println(q);
+
+      Scanner scan =new Scanner (System.in);
+      System.out.print("Enter number: \t");
+      int num = scan.nextInt();
+      System.out.println("number is:"+num );
+
+
 
           }
       }
