3dc7825f715501663fcdd8d1caebc6af4e8b4fc3
seyran7 <brsr888@gmail.com>
2024-12-11 22:39:15 +0400

    i add new code

---
+++ src/main/java/Turing/ArraySumApp.java
@@ -0,0 +1,30 @@
+package Turing;
+
+import java.util.Scanner;
+
+public class ArraySumApp {
+
+    public static void main(String[]args){
+
+        while(true) {
+
+            Scanner scan = new Scanner(System.in);
+
+            System.out.println("Enter a number for array's size:");
+
+            int size = scan.nextInt();
+
+            int[] array = new int[size];
+
+            System.out.println("Enter numbers for to fill in the array:");
+
+            int sum=0;
+
+            for( int i = 0; i<array.length;i++){
+               array[i]=scan.nextInt();
+                sum+=array[i];
+            }
+            System.out.println(sum);
+        }
+    }
+}
