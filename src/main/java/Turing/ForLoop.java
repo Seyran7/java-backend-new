4104dd1941a269959d6d060976959423b045493f
seyran7 <brsr888@gmail.com>
2025-01-04 22:50:09 +0400

    I write a code for practice

---
+++ src/main/java/Turing/ForLoop.java
@@ -0,0 +1,26 @@
+package Turing;
+
+import java.util.Scanner;
+
+public class ForLoop {
+    public static void main(String[] args) {
+        while (true) {
+            //input
+            Scanner scan = new Scanner(System.in);
+            System.out.println("Enter a number:");
+            int num = scan.nextInt();
+            //proses;this code will calculate all number's sum in entering number if it's even
+                int cem = 0;
+                for (int i = 0; i <= num; i++) {
+                    if (i % 2 == 0) {
+                        cem += i;
+
+                    }
+                }
+                System.out.println(cem);
+        }
+
+    }
+
+}
+
