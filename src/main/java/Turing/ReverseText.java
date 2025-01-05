844e258723dd13fe286eb0d883b8a44f6f69c374
seyran7 <brsr888@gmail.com>
2025-01-05 22:09:13 +0400

    I write a code every night for practice

---
+++ src/main/java/Turing/ReverseText.java
@@ -0,0 +1,20 @@
+package Turing;
+
+import java.util.Scanner;
+
+public class ReverseText {
+    public static void main(String[] args) {
+        //input
+        while(true) {
+            Scanner scan = new Scanner(System.in);
+            System.out.println("Bir metin elave edin:");
+            String text = scan.nextLine();
+            //proses This code will reverse the text which is entered in console
+            String reversedText = "";
+            for (int i = text.length() - 1; i >= 0; i--) {
+                reversedText += text.charAt(i);
+            }
+            System.out.println(reversedText);
+        }
+    }
+}
