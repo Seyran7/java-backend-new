7c0b4a4be440c253b0e885d9030cac601ed0a93c
seyran7 <brsr888@gmail.com>
2025-01-06 21:50:08 +0400

    I add my today's code for practice

--- src/main/java/Turing/TheLongestName.java
+++ src/main/java/Turing/TheLongestName.java
@@ -4,18 +4,23 @@ import java.util.Scanner;
 
 public class TheLongestName {
     public static void main(String[] args) {
+        LongestName();
+    }
         //input
+        public static void LongestName(){
         Scanner scan =new Scanner(System.in);
         //proses this code will show the longest entered name
+
         String enteredName="";
         String longestName="";
-        while(!(enteredName.equals("Q")||enteredName.equals("q"))){
+        while(!(enteredName.equals("Q")||enteredName.equals("q"))) {
             System.out.println("Enter a name:\nif you want to quit enter 'Q' or 'q':");
-            enteredName=scan.nextLine();
-            if(enteredName.length()>=longestName.length()){
-                longestName=enteredName;
+            enteredName = scan.nextLine();
+            if (enteredName.length() >= longestName.length()) {
+                longestName = enteredName;
             }
             System.out.println(longestName);
         }
     }
 }
+
